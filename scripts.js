cb48a7f576cbb093cbb9d03ab7596db0f8250fba
Marcos Vinicius <marcos.mva18@hotmail.com>
2021-09-20 14:23:51 -0300

    salvando projetos

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
-var listaFilmes = ["https://static.metacritic.com/images/products/movies/9/e97f53a83fc8164369f954313e451e74.jpg", "https://upload.wikimedia.org/wikipedia/pt/8/88/ButtonPoster.jpg", "https://upload.wikimedia.org/wikipedia/pt/a/a6/Once_Upon_a_Time_in_Hollywood_poster.png"]
+function adicionarFilme() {
+    var filmeFavorito = document.getElementById("filme").value
+    if (filmeFavorito.endsWith(".jpg")){
+        listarFilmesNaTela(filmeFavorito) // os parenteses envia as variáreis para outra função
+        } else {
+    console.error("Endereço de filme inválido")
+}
+    document.getElementById("filme").value = ""
+}
 
-listaFilmes.push("https://br.web.img2.acsta.net/medias/nmedia/18/90/43/36/20096333.jpg")
-
-for (var indice = 0; indice < listaFilmes.length; indice++) {
-    document.write("<img src=" + listaFilmes[indice] + ">")
-    }
+function listarFilmesNaTela(filme) { // os parenteses podem armazenar as variáveis de outros lugares
+    var elementoFilmeFavorito = "<img src=" + filme + ">"
+    var elementoListaFilmes = document.getElementById("listaFilmes")
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito
+}
